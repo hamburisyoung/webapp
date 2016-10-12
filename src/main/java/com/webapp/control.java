f85117d8d2b6221b9e1ce02f0e78f43f3822c8aa
hambur <hambur@hamburdeMacBook-Pro.local>
2016-10-12 14:58:29 +0800

    Initial commit

---
+++ src/main/java/com/webapp/control.java
@@ -0,0 +1,15 @@
+package com.webapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+/**
+ * Created by hambur on 2016/10/12.
+ */
+@Controller
+public class control {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "index";
+
+}
